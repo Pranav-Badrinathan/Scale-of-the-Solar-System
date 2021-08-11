aa43300863f4c40a673bad4c6dbdab8f09a3be44
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2021-08-11 10:16:05 -0300

    Added a simple scale comparision

--- js/main.js
+++ js/main.js
@@ -13,12 +13,26 @@ $(function(){
 		$(this).toggleClass("stop");
 		toggleAnim();
 	});
+	
+	$(".change").on("click", function(){
+		if(!mainSet.scale == 0) {
+			mainSet.scale = 0;
+			scaleSet.scale = 1;
+		}
+		else{
+			mainSet.scale = 1;
+			scaleSet.scale = 0;
+		}
+	});
+
+	makeScalePlanets();
 });
 
 function initTwo(){
 	var elem = $("#clickable").get()[0];
 	window.two = new Two({ fullscreen: true, autostart: true }).appendTo(elem);
 	window.mainSet = two.makeGroup();
+	window.scaleSet = two.makeGroup();
 	mainSet.translation.set(two.width/2, two.height/2);
 }
 
@@ -43,4 +57,27 @@ function makeOrbits(){
 		orbit.linewidth = e*2;
 		mainSet.add(orbit);
 	});
+}
+
+function makeScalePlanets(){
+	var scaledPlanets = [2.439, 6.052, 6.371, 3.389, 69.911, 58.232, 25.362, 24.622, 696.340];
+
+	var distFromPrv = 1;
+
+	scaledPlanets.forEach((e, i) => {
+		var xpos = (i)* two.width/50 + distFromPrv * 10
+		var planet = two.makeCircle(xpos, 0, e * Math.min(two.width, two.height)/500);
+
+		planet.stroke = "none";
+		scaleSet.add(planet);
+
+		//change this later pls. It's sooooo UGLY!
+		if(i<3) distFromPrv += 0;
+		else if(i<4) distFromPrv += 9
+		else if(i<5) distFromPrv += 17
+		else if (i<6) distFromPrv += 10
+		else if (i<7) distFromPrv += 6
+		else if (i<8) distFromPrv += 100
+	})
+	scaleSet.scale = 0;
 }
